fe859f25549a59c7f116ca4f3e0a96e78687fea8
unknown <adriano.contacto@gmail.com>
2021-02-07 17:33:52 -0300

    Agrega mas test
    Agrega test en caso de que el dato ingresado sea correcto

--- tareas/clase-5/Tarea-3/test.js
+++ tareas/clase-5/Tarea-3/test.js
@@ -1,18 +1,24 @@
 function testValidarHoras(){
     console.assert(validarHoras('') === 'Completa todas las horas con numeros positivos',
     'validar horas no valido que se haya ingresado un dato correcto')
+    console.assert(validarHoras(2) === '', 
+    'No se puso validar una hora valida')
 }
 
 function testValidarMinutos(){
     console.assert(
         validarMinutos('') === 'Completa todos los minutos con numeros positivos',
     'validar minutos no valido que se haya ingresado un dato correcto')
+    console.assert(validarMinutos(2) === '', 
+    'No se puso validar un minuto valido')
 }
 
 function testValidarSegundos(){
     console.assert(
         validarSegundos('') === 'Completa todos los segundos con numeros positivos',
     'validar segundos no valido que se haya ingresado un dato correcto')
+    console.assert(validarSegundos(2) === '', 
+    'No se puso validar un segundo valido')
 }
 
 testValidarHoras()
