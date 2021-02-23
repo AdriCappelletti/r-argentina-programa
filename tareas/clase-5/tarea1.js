af0382bc9ed2bf6d2a70c7a58ab07df341b53886
unknown <adriano.contacto@gmail.com>
2021-02-23 20:50:42 -0300

    Agrega validación
    Agrega una RegEx para validar si el usuario ingreso un numero

--- tareas/clase-5/tarea1.js
+++ tareas/clase-5/tarea1.js
@@ -4,6 +4,10 @@ $botonCalcular.onclick = function(){
   let salarioAnual = Number(document.querySelector('#salario-anual').value)
   let salarioMensual = salarioAnual / 12
 
+  if (!/^[0-9]+/.test(salarioAnual)) {
+    return alert('Ingrese un numero valido por favor')
+  }
+
   document.querySelector('#salario-mensual').value = Math.round(salarioMensual)
   return false;
 }
